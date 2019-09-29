9ef389ef5015f793be6a5f93f90ad9639fe37108
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-09-29 23:26:21 +0600

    Five unique numbers are generated on loading application and on button's click.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import Ball from "./Ball/Ball";
 import './App.css';
 
 class App extends Component {
-    state = {numbers: [0, 0, 0, 0, 0]};
+    state = {numbers: []};
+    componentDidMount() {
+        window.addEventListener('load', this.changeNumbers);
+    }
     changeNumbers = () => {
-        const numbers = [1, 1, 1, 1, 1];
+        let numbers = this.generateLottoNumbers();
         this.setState({numbers});
+    };
+    generateLottoNumbers = () => {
+        let numbers = [];
+        while (numbers.length < 5) {
+            let number;
+            let index;
+            number = this._getRandomIntInclusive(1, 36);
+            if (numbers.length === 0) {
+                numbers.push(number);
+            } else {
+                index = numbers.indexOf(number);
+            }
+            if (index === (-1)) {
+                numbers.push(number);
+            }
+        }
+        numbers.sort((a, b) => a - b);
+        return numbers;
+    };
+    _getRandomIntInclusive = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     };
     render() {
         return (
