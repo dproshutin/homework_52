b0ab7181faf5d86fede7fe354b3bd5a186297d79
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-09-29 17:01:58 +0600

    A prototype with 5 predefined numbers is created. By clicking the button, all values are changed from 0 to 1.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import Ball from "./Ball/Ball";
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <div className={"btn"}>
-            <button>New numbers</button>
-          </div>
-            <div className="lotto">
-                <Ball value={"1"}/>
+    state = {numbers: [0, 0, 0, 0, 0]};
+    changeNumbers = () => {
+        const numbers = [1, 1, 1, 1, 1];
+        this.setState({numbers});
+    };
+    render() {
+        return (
+            <div className="App">
+              <div className={"btn"}>
+                <button onClick={this.changeNumbers}>New numbers</button>
+              </div>
+                <div className="lotto">
+                    <Ball value={this.state.numbers[0]}/>
+                    <Ball value={this.state.numbers[1]}/>
+                    <Ball value={this.state.numbers[2]}/>
+                    <Ball value={this.state.numbers[3]}/>
+                    <Ball value={this.state.numbers[4]}/>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
